95185226609f6f9204b02bdf8fc00802358b5688
john1015 <10151404john@gmail.com>
2024-11-11 11:01:09 +0900

    11/11 thymeleaf

---
+++ SpringThymeLeafProject/src/main/java/com/sist/web/entity/Emp.java
@@ -0,0 +1,26 @@
+package com.sist.web.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+// 클래스명과 테이블명이 동일하면 => table명을 지정하지 않아도 된다
+@Entity
+@Data
+public class Emp {
+	@Id
+	private int empno;
+	private String ename,job,hiredate,comm,mgr;
+	// csv => 날짜 , 값중에 null이 있는 경우 => 문자열 (text)
+	private int sal;
+	@Column(insertable = false , updatable = false)
+	private int deptno;
+	@ManyToOne(fetch = FetchType.EAGER)
+	// LAZE(지연) , EAGER(즉시 로딩)
+	@JoinColumn(name = "deptno",referencedColumnName = "deptno")
+	private Dept dept;
+	
+}
